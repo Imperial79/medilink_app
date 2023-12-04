974cede0030744e0dc9da6c5a5342979e7eaf1a2
Vivek Verma <vivekdgp01@gmail.com>
2023-12-04 13:57:26 +0530

    MM

--- lib/screens/Auth/registerUI.dart
+++ lib/screens/Auth/registerUI.dart
@@ -1,6 +1,4 @@
 import 'dart:convert';
-
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:medilink/dashboardUI.dart';
